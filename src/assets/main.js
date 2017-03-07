b69969c69537dfb78fb279d1498d3fee7f49e1ea
Seimon Arcinue <dzgarcinue@gmail.com>
2017-03-07 09:01:49 -0800

    jQuery solved

--- src/assets/main.js
+++ src/assets/main.js
@@ -4,9 +4,37 @@ $(function() {
     url: 'https://www.codeschool.com/users/s31m0n.json',
     dataType: 'jsonp',
     success: function(response) {
-      console.log('response', response);
+      addCourses(response.courses.completed);
 
     }
   });
+
+  function addCourses(courses) {
+
+    var $badges = $('#badges');
+
+    courses.forEach(function(course){
+
+      var $course = $('<div />', {
+        'class' : 'course'
+      }).appendTo($badges);
+
+      $('<h3 />', {
+        text: course.title
+      }).appendTo($course);
+
+      $('<img />', {
+        src: course.badge
+      }).appendTo($course);
+
+      $('<a />', {
+        'class': 'btn btn-primary',
+        target: '_blank',
+        href: course.url,
+        text: 'See Course'
+      }).appendTo($course)
+    })
+
+  }
 
 });
